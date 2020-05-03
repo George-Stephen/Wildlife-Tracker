a95170ded49ff9cc2bca0dff5a43d9b8e6fea6ae
George-Stephen <yazz777stevie@gmail.com>
2020-05-03 15:10:49 +0300

    deployment

--- src/main/java/models/DB.java
+++ src/main/java/models/DB.java
@@ -5,6 +5,6 @@ import org.sql2o.*;
 public class DB{
     // private static String connectionString = "jdbc:postgresql://localhost:5432/wildlife_tracker";
      // public static Sql2o sql2o = new Sql2o(connectionString, "stephen", "stephen2020");//
-    private static String connectionString = "postgres://dksgitbwtewwbm:5603f2f69ea00a8bc9d17682f170874eb20ac570bc583bf3557dea5cb9d2a912@ec2-52-6-143-153.compute-1.amazonaws.com:5432/dfjhee4vn5bjve\n";
-    public static Sql2o sql2o = new Sql2o(connectionString, "dksgitbwtewwbm", "5603f2f69ea00a8bc9d17682f170874eb20ac570bc583bf3557dea5cb9d2a912\n");
+    private static String connectionString = "jdbc:postgresql://ec2-52-87-135-240.compute-1.amazonaws.com:5432/dcgo8sr0t94hgf";
+    public static Sql2o sql2o = new Sql2o(connectionString, "axenptosipwifu", "83443395ecafa46edbd7868a81b631c69f7ee08aa8313a4b223bf4c720aacc31");
 }
